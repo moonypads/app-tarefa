0b6fdc535cee4c96b2d8935d1f0a47cc7d1a4180
Isabele Ferreira <isabele.f@escolar.ifrn.edu.br>
2023-12-29 07:52:20 -0300

    funcionando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdicionarTarefaComponent } from './adicionar-tarefa/adicionar-tarefa.component';
-import { EditarTarefaComponent } from './editar-tarefa/editar-tarefa.component';
 import { ExibirTarefaComponent } from './exibir-tarefa/exibir-tarefa.component';
 
 const routes: Routes = [
   { path: 'adicionar', component: AdicionarTarefaComponent},
   { path: 'exibir', component: ExibirTarefaComponent},
   { path: '', redirectTo: '/adicionar', pathMatch: 'full'},
-  { path: 'editar', component: EditarTarefaComponent}
+  
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,13 @@ import { AppComponent } from './app.component';
 import { AdicionarTarefaComponent } from './adicionar-tarefa/adicionar-tarefa.component';
 import { ExibirTarefaComponent } from './exibir-tarefa/exibir-tarefa.component';
 import { FormsModule } from '@angular/forms';
-import { EditarTarefaComponent } from './editar-tarefa/editar-tarefa.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     AdicionarTarefaComponent,
     ExibirTarefaComponent,
-    EditarTarefaComponent
+
   ],
   imports: [
     BrowserModule,
